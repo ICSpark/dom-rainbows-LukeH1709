06410ff3ad3c6c904c1ad0bc067bf782db93c409
Luke Heisler <12lukeheisler@gmail.com>
2025-03-01 20:39:20 +0000

    finished

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Task 1: Change the cloud to stop the rain
 // 1. Create a variable "cloud" that stores the element "cloud"
-
+let cloud = document.getElementById("cloud");
+cloud.src = "images/cloud.png";
 
 
 // 2. Change the src from "images/rain-cloud.png" to "images/cloud.png"
@@ -9,7 +10,8 @@
 
 // Task 2: Make the rainbow visible
 // 1. Create a variable "rainbow" that holds the whole rainbow
-
+let rainbow = document.getElementById("rainbow");
+rainbow.style.display = "flex";
 
 
 // 2. Change the css value of display to "flex" to make the rainbow appear
@@ -19,7 +21,8 @@
 // 1. Create a variable called "arches" that will store all the arches in a rainbow
 // Use document.getElementsByClassName(); to make an array of all values that have the class arch
 // These elements are stored in the order they are seen from top to bottom
-
+let arches = document.getElementsByClassName("arch");
+arches[0].style.backgroundColor = "red";
 
 // 2. The first arch in the rainbow is green! Change it to red!
 // Do this by changing the backgroundColor property of the first arch to "red"!
@@ -29,7 +32,9 @@
 
 // Task 4: Add missing violet arch
 // 1. Create new arch piece
-
+let violetArch = document.createElement("div");
+violetArch.className = "arch violet";
+rainbow.appendChild(violetArch);
 
 // 2. Give it both classes: 'arch' for shape and 'violet' for color
 
